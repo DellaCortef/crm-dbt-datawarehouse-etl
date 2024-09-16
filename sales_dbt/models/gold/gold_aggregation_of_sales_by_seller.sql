db9061076d532336ef46f0048d8be6565beaf4aa
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-16 15:47:27 -0300

    adding the parameters in ORDER BY

--- sales_dbt/models/gold/gold_aggregation_of_sales_by_seller.sql
+++ sales_dbt/models/gold/gold_aggregation_of_sales_by_seller.sql
@@ -1,18 +1,17 @@
 WITH sales_by_seller AS (
     SELECT 
         email AS seller, 
-        DATE(date_day) AS date_day, 
+        date_day, 
         product_value, 
         product_quantity, 
         product_type 
     FROM 
-        {{ ref('silver_sales') }}  
+        {{ ref('silver_sales')}} 
     WHERE 
         product_value < 6000 
         AND date_day >= '2024-09-01' 
         AND date_day <= '2024-09-11'
     )
-
     sales_aggregated_seller AS (
         SELECT 
             seller, 
@@ -26,7 +25,6 @@ WITH sales_by_seller AS (
             seller, 
             date_day
     )
-
     SELECT 
         seller, 
         date_day, 
