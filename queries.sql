9fa2ebb9cef0a781efeab1e15fbd417da23538a3
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-15 21:58:32 -0300

    applying sql sintaxe

--- queries.sql
+++ queries.sql
@@ -1,73 +1,73 @@
 -- last week Sales
-        SELECT 
-            DATE(date_time) AS date_day, 
-            SUM(product_value) AS total_sales, 
-            COUNT(*) AS total_transactions
-        FROM 
-            sales 
-        WHERE 
-            date_time >= CURRENT_DATE - INTERVAL '6 days' 
-            AND date_time < CURRENT_DATE + INTERVAL '1 day' 
-        GROUP BY 
-            DATE(date_time) 
-        ORDER BY 
-            date_day DESC
+SELECT 
+    DATE(date_time) AS date_day, 
+    SUM(product_value) AS total_sales, 
+    COUNT(*) AS total_transactions
+FROM 
+    sales 
+WHERE 
+    date_time >= CURRENT_DATE - INTERVAL '6 days' 
+    AND date_time < CURRENT_DATE + INTERVAL '1 day' 
+GROUP BY 
+    DATE(date_time) 
+ORDER BY 
+    date_day DESC
     
 -- sales with price value lesse than 6k
-        SELECT 
-            email, 
-            date_time, 
-            product_value, 
-            product_quantity, 
-            product_type 
-        FROM 
-            sales 
-        WHERE 
-            product_value < 6000 
-            AND date_time >= '2024-09-01' 
-            AND date_time <= '2024-09-11'
-        ORDER BY 
-            date_time ASC, 
-            product_value DESC
+SELECT 
+    email, 
+    date_time, 
+    product_value, 
+    product_quantity, 
+    product_type 
+FROM 
+    sales 
+WHERE 
+    product_value < 6000 
+    AND date_time >= '2024-09-01' 
+    AND date_time <= '2024-09-11'
+ORDER BY 
+    date_time ASC, 
+    product_value DESC
 
 -- aggregation of Sales by Day and Product
-        WITH filtered_sales AS (
-            SELECT 
-                email, 
-                DATE(date_time) AS date_day, 
-                product_value, 
-                product_quantity, 
-                product_type 
-            FROM 
-                sales 
-            WHERE 
-                product_value < 6000 
-                AND date_time >= '2024-09-01' 
-                AND date_time <= '2024-09-11'
-        ),
-        aggregated_sales AS (
-            SELECT 
-                date_day, 
-                product_type, 
-                SUM(product_value) AS total_value, 
-                SUM(product_quantity) AS total_quantity,
-                COUNT(*) AS total_sales
-            FROM 
-                filtered_sales
-            GROUP BY 
-                date_day, 
-                product_type
-        )
-        SELECT 
-            date_day, 
-            product_type, 
-            total_value, 
-            total_quantity, 
-            total_sales
-        FROM 
-            aggregated_sales
-        ORDER BY 
-            date_day ASC, product_type ASC;
+WITH filtered_sales AS (
+    SELECT 
+        email, 
+        DATE(date_time) AS date_day, 
+        product_value, 
+        product_quantity, 
+        product_type 
+    FROM 
+        sales 
+    WHERE 
+        product_value < 6000 
+        AND date_time >= '2024-09-01' 
+        AND date_time <= '2024-09-11'
+),
+aggregated_sales AS (
+    SELECT 
+        date_day, 
+    product_type, 
+    SUM(product_value) AS total_value, 
+    SUM(product_quantity) AS total_quantity,
+    COUNT(*) AS total_sales
+    FROM 
+        filtered_sales
+    GROUP BY 
+        date_day, 
+        product_type
+)
+SELECT 
+    date_day, 
+    product_type, 
+    total_value, 
+    total_quantity, 
+    total_sales
+FROM 
+    aggregated_sales
+ORDER BY 
+    date_day ASC, product_type ASC;
     
 -- aggregation of Sales by seller
 WITH sales_by_seller AS (
