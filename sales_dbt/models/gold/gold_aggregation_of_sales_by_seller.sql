3fb2f2684aa5e85ab82d44397c4c9dc045cdad19
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-16 15:19:00 -0300

    creating our gold views

---
+++ sales_dbt/models/gold/gold_aggregation_of_sales_by_seller.sql
@@ -0,0 +1,36 @@
+WITH sales_by_seller AS (
+    SELECT 
+        email AS seller, 
+        DATE(data) AS date_day, 
+        product_value, 
+        product_quantity, 
+        product_type 
+    FROM 
+        {{ ref('silver_sales') }}  
+    WHERE 
+        product_value < 6000 
+        AND data >= '2024-09-01' 
+        AND data <= '2024-09-11'
+    ),
+    vendas_aggregated_vendedor AS (
+        SELECT 
+            seller, 
+            date_day, 
+            SUM(product_value) AS total_value, 
+            SUM(product_quantity) AS total_quantity, 
+            COUNT(*) AS total_sales
+        FROM 
+            sales_by_seller
+        GROUP BY 
+            seller, 
+            date_day
+    )
+    SELECT 
+        seller, 
+        date_day, 
+        total_value, 
+        total_quantity, 
+        total_vendas
+    FROM 
+        sales_aggregated_seller
+    ORDER BY 
